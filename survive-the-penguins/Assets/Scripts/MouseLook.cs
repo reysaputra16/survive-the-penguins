960f2da34babd9d72473ffc23f0c614f2fc736c3
reysaputra16 <rjsjkt@gmail.com>
2020-01-23 20:49:53 +0100

    playerScript with movement and jump

--- survive-the-penguins/Assets/Scripts/MouseLook.cs
+++ survive-the-penguins/Assets/Scripts/MouseLook.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Makes the mouse cursor disappear.
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Rotation is with "-", because "+" makes the mouseLook inverted.
+        xRotation -= mouseY;
+        //Limit rotation for player along the x-axis.
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+       
     }
 }
