6f5aa686c7daff7565ddde422aa8eb1b5329e7fa
Lynnrs <56801788+Lynnrs@users.noreply.github.com>
2020-01-23 22:12:04 +0100

    maze floor

---
+++ survive-the-penguins/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Maze mazePrefab;
+    private Maze mazeInstance;
+    // Start is called before the first frame update
+    private void Start() {
+        BeginGame();
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            RestartGame();
+        }
+    }
+
+    private void BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        mazeInstance.Generate();
+    }
+
+    private void RestartGame()
+    {
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
